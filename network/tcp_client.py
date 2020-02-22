fff6a1f370106cdc2a9d2d9edbefee1d661890ea
sin91 <sin91@126.com>
2020-02-23 01:47:48 +0800

    Correct server to client

--- network/tcp_client.py
+++ network/tcp_client.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+
 import socket
 
-def connect_server():
-    print('Connecting to server...')
+def start_udp_client():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    for data in ['Devin', 'Pall', 'Jinny']:
+        # 发送数据 UDP发送数据不需要先connect
+        sock.sendto(data,('127.0.0.1', 8899))
 
-    # 定义使用IPV4协议和传输流的socket对象
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # 建立连接
-    s.connect(('127.0.0.1', 8899))
-
-    # 接收欢迎信息
-    print(s.recv(1024).decode('utf-8'))
-    for data in [b'Michael', b'Tracy', b'Sarah']:
-        # 发送数据
-        s.send(data)
-        print(s.recv(1024).decode('utf-8'))
-
-    s.send(b'exit')
-    s.close()
+        # 接收数据
+        print(sock.recv(1024).decode('utf-8'))
+    socket.close()
 
 if __name__ == '__main__':
-    connect_server()
+    start_udp_client()
